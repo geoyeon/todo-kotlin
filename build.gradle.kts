8592239130308a8fba076f13a32772b7ab23981b
geoyeon <geoyeon@gmail.com>
2025-08-16 00:22:51 +0900

    test: 테스트 코드 추가
    - 생성 및 목록 조회만

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
